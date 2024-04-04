4e0d9d1724e73adf395dd749520edf3d473c74c8
LaitAh <lea.clair34@gmail.com>
2024-04-04 19:14:54 +0200

    creation of a modal to inform the user of the end of the countdown

---
+++ project-code/src/components/ModalTimerEnd.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="modal" v-if="show">
+    <div class="modal-content">
+      <audio ref="audio" src="../assets/song-timerEnd.mp3" loop></audio>
+      <h2>Time's up! Your tea is ready.</h2>
+      <button class="close" @click="close">&times;</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      show: false
+    };
+  },
+  methods: {
+    open() {
+      this.show = true;
+      this.$nextTick(() => {
+        this.$refs.audio.addEventListener('loadedmetadata', () => {
+          this.$refs.audio.play();
+        });
+      });
+    },
+    close() {
+      this.show = false;
+    }
+  }
+};
+</script>
+
+<style scoped>
+.modal {
+  display: flex;
+  justify-content: center;
+}
+
+.modal-content {
+  width: 50vw;
+  border: 1px solid #ccc;
+  border-radius: 10px;
+  overflow: hidden;
+  margin: 10px;
+  text-align: center;
+}
+
+button {
+  width: 100%;
+  padding: 0.25em 1em;
+  background-color: #FF7A45;
+  color: white;
+  font-weight: 600;
+  font-size: 2em;
+  border: none;
+}
+</style>
+  
